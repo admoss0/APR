bd09eaaa0ba4fa82e82f1dabc6da1d3b54b8e4b6
David Moss <admoss0@gmail.com>
2015-01-03 17:54:50 +1000

    initial push

---
+++ src/main/java/info/admoss/apr/Organisation.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package info.admoss.apr;
+
+/**
+ *
+ * @author admos0
+ */
+public class Organisation {
+    public Organisation() {
+        System.out.println("Hello World");
+    }
+}
