690af26612f36c568e6fb5ae434e9f06ba4cf1c6
David Moss <admoss0@gmail.com>
2015-01-03 20:28:42 +1000

    known good build

--- src/main/java/info/admoss/apr/Organisation.java
+++ src/main/java/info/admoss/apr/Organisation.java
@@ -17,19 +17,85 @@ import com.hp.hpl.jena.rdf.model.Resource;
  */
 public class Organisation {
     
-    Model m;
-    Resource r;
-    Property title, description, website;
+    private Model m;
+    private Resource r;
+    private Property title;
+    private Property description;
+    private Property website;
     
     public Organisation() {
         
         m = ModelFactory.createDefaultModel();
-        
+        setProperties();
+        r = m.createResource("http://admoss.info/apr/org/org#103");
+        r.addProperty(title, "Liberty and Democracy Party" );
+        r.addProperty(description, "\"The Liberty & Democracy Party (formerly the Liberal Democratic Party) stands for lower taxes, small government and individual responsibility.\"");
+        r.addProperty(website, "www.ldp.org.au");
         SOPL("Organisation initialised");
         
     }
     
+    
+    
+    
+   
+    
     private void SOPL(String in){
         System.out.println("SOPL: " + in);
     }
+
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return r.getProperty(title).getObject().toString();
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description.toString();
+    }
+
+    /**
+     * @return the website
+     */
+    public String getWebsite() {
+        return website.toString();
+    }
+
+    /**
+     * @param title the title to set
+     */
+    public void setTitle(Property title) {
+        this.title = title;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(Property description) {
+        this.description = description;
+    }
+
+    /**
+     * @param website the website to set
+     */
+    public void setWebsite(Property website) {
+        this.website = website;
+    }
+    
+    
+    final void setProperties() {
+        
+      title = m.createProperty("http://purl.org/dc/elements/1.1/title");
+      description = m.createProperty("http://purl.org/dc/elements/1.1/description");
+      website = m.createProperty("http://xmlns.com/foaf/0.1/homepage");
+      
+        
+    }
 }
+
+
+
