aed286bae7dccb945461a44e57e04b218e6e2095
admoss0 <admoss0@gmail.com>
2015-01-14 13:36:32 +1000

    added loglevel to Organisation

--- src/main/java/info/admoss/apr/OrgList.java
+++ src/main/java/info/admoss/apr/OrgList.java
@@ -3,34 +3,54 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package info.admoss.apr;
 
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.rdf.model.Model;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import org.apache.log4j.Logger;
 
 /**
  *
  * @author admos0
  */
 public class OrgList {
-    
-    
-    public OrgList(){
-        
-        List<OrgListMember> list = new ArrayList<>();
-          
+
+    List<OrgListMember> list;
+    private String endpoint = "http://semantic-admoss.rhcloud.com/ds/query";
+    Model m;
+    Logger log = Logger.getLogger(Organisation.class);
+
+    public OrgList() {
+
+        list = new ArrayList<>();
+
     }
-    
-    
+
+    private void retrieve() {
+        log.debug("starting retrieve for ");
+        String query = "construct { ?s ?p ?o } where { ?s ?p ?o ."
+                + " }";
+        Query q;
+        q = QueryFactory.create(query);
+        QueryExecution qe = QueryExecutionFactory.createServiceRequest(endpoint, q);
+        m = qe.execConstruct();
+        log.debug("retrieve completed");
+    }
+
     class OrgListMember {
+
         private String id;
         private String title;
-        
+
         @Override
-        public String toString(){
-         return this.getTitle();   
+        public String toString() {
+            return this.getTitle();
         }
 
         /**
@@ -61,6 +81,5 @@ public class OrgList {
             this.title = title;
         }
     }
-    
-    
+
 }
