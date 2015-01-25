b8a5bf7e5782aae5d0647d863b29530aad9bb359
admoss0 <admoss0@gmail.com>
2015-01-25 16:17:27 +1000

    change to gui

--- src/main/java/info/admoss/apr/Organisation.java
+++ src/main/java/info/admoss/apr/Organisation.java
@@ -25,28 +25,28 @@ public class Organisation {
 
     private String endpoint = "http://semantic-admoss.rhcloud.com/ds/query";
     private String id;
-    private Model m;
-    private Resource r;
+    private Model m, oldModel;
+    private Resource r, oldR;
     private Property title;
     private Property description;
     private Property website;
+    private Property oldTitle;
+    private Property oldDescription;
+    private Property oldWebsite;
     private Level loglevel = Level.DEBUG;
     Logger log = Logger.getLogger(Organisation.class);
-    
-    
-   
-    
 
     public Organisation() {
-      log.setLevel(loglevel);
+        log.setLevel(loglevel);
         m = ModelFactory.createDefaultModel();
         setProperties();
-        testOrg();
+
     }
 
     public Organisation(String input) {
         log.setLevel(loglevel);
         m = ModelFactory.createDefaultModel();
+        oldModel = ModelFactory.createDefaultModel();
         setProperties();
         setId(input);
     }
@@ -58,7 +58,7 @@ public class Organisation {
      */
     private void SOPL(String in) {
         System.out.println("SOPL: " + in);
-        
+
     }
 
     /**
@@ -85,9 +85,9 @@ public class Organisation {
     /**
      * @param title the title to set
      */
-    public void setTitle(Property title) {
+    public void setTitle(String thing) {
         r.removeAll(this.title);
-        r.addProperty(this.title, title);
+        r.addProperty(this.title, thing);
     }
 
     /**
@@ -111,8 +111,10 @@ public class Organisation {
      */
     final void setProperties() {
         title = m.createProperty("http://purl.org/dc/elements/1.1/title");
+        oldTitle = oldModel.createProperty("http://purl.org/dc/elements/1.1/title");
         description = m.createProperty("http://purl.org/dc/elements/1.1/description");
-        website = m.createProperty("http://xmlns.com/foaf/0.1/homepage");
+        oldDescription = oldModel.createProperty("http://purl.org/dc/elements/1.1/description");
+        oldWebsite = oldModel.createProperty("http://xmlns.com/foaf/0.1/homepage");
     }
 
     /**
@@ -145,22 +147,29 @@ public class Organisation {
     }
 
     private void retrieve() {
-        log.debug("starting retrieve for " + id);
+        // log.debug("starting retrieve for " + id);
         String query = "construct { <" + id + "> ?p ?o } where { <" + id + "> ?p ?o }";
         Query q;
         q = QueryFactory.create(query);
         QueryExecution qe = QueryExecutionFactory.createServiceRequest(endpoint, q);
         m = qe.execConstruct();
+        oldModel = m;
         r = m.getResource(id);
-        log.debug("retrieve completed");
+        oldR = oldModel.getResource(id);
+        //log.debug("retrieve completed");
     }
 
     private void testOrg() {
-  
+
         r = m.createResource("http://admoss.info/apr/org/org#103");
         r.addProperty(title, "Liberty and Democracy Party");
         r.addProperty(description, "\"The Liberty & Democracy Party (formerly the Liberal Democratic Party) stands for lower taxes, small government and individual responsibility.\"");
         r.addProperty(website, "www.ldp.org.au");
-    log.debug("Test Organisation initialised");
+        log.debug("Test Organisation initialised");
     }
+
+    public void sendOrg() {
+
+    }
+
 }
